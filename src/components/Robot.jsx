0ee10df78497103c9e28616d18516af9e56bd374
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-02-01 01:06:28 +0500

    Deploying

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 export default function Robot() {
       return (
             <div className="relative w-full h-full">
-                  <div className="absolute bottom-0 w-full h-[12%] bg-[#18181b]"></div>
+                  <div className="absolute bottom-0 w-screen h-[12%] bg-[#18181b]"></div>
                   <Spline scene="https://prod.spline.design/DP9cMa9ptmLGPE2D/scene.splinecode" />;
             </div>
       );
--- src/components/VR.jsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function VR() {
-  return (
-    <Spline scene="https://prod.spline.design/pge-6no2BrvioyAn/scene.splinecode" />
-  );
-}
